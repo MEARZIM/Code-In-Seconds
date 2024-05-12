1b6a0cf06742cab6de56952040cd4daa861c9e65
MEARZIM <asahaayan@gmail.com>
2024-05-12 12:03:53 +0530

    fix: Fixed deployment bug - phase 2

--- components/dashboard/PostFeed/commentFeed.tsx
+++ components/dashboard/PostFeed/commentFeed.tsx
@@ -1,4 +1,4 @@
-import CommentItem from './CommentItem';
+// import CommentItem from './CommentItem';
 
 interface CommentFeedProps {
   comments?: Record<string, any>[];
@@ -7,9 +7,9 @@ interface CommentFeedProps {
 const CommentFeed: React.FC<CommentFeedProps> = ({ comments = [] }) => {
   return (
     <>
-      {comments.map((comment: Record<string, any>,) => (
+      {/* {comments.map((comment: Record<string, any>,) => (
         <CommentItem key={comment.id} data={comment} />
-      ))}
+      ))} */}
     </>
   );
 };
